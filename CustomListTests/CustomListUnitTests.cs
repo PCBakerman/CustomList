472a7adc693fe321b30cf3fbd1f5aaf0a6f14284
PCBakerman <72618089+PCBakerman@users.noreply.github.com>
2020-10-28 15:40:37 -0500

    Added 3 Remove tests and the Remove function

--- CustomListTests/CustomListUnitTests.cs
+++ CustomListTests/CustomListUnitTests.cs
@@ -10,7 +10,7 @@ namespace CustomListTests
     //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
     [TestClass]
     public class CustomListUnitTests
-    { 
+    {
         [TestMethod]
         public void Add_AddItemToEmptyList_ItemGoesToIndexZero()
         {
@@ -27,21 +27,21 @@ namespace CustomListTests
             // Assert
             Assert.AreEqual(expected, actual);
         }
-        
+
         [TestMethod]
         public void Add_AddItemToEmptyList_CountIncrementsToOne()
         {
             // Arrange
-           
+
             CustomList<int> testList = new CustomList<int>();
             int item = 10;
             int expected = 1;
-            int actual; 
+            int actual;
 
             // Act
             testList.Add(item);
             actual = testList.Count; // error expected until "Count" is added to class
-             
+
             // Assert
             Assert.AreEqual(expected, actual);
         }
@@ -53,7 +53,7 @@ namespace CustomListTests
             int firstItem = 3;
             int item = 10;
             int expected = 3;
-            int actual; 
+            int actual;
 
             // Act
             testList.Add(firstItem);
@@ -61,7 +61,7 @@ namespace CustomListTests
             actual = testList[0]; // error expected until "Count" is added to class
             // Assert
             Assert.AreEqual(expected, actual);
-            
+
 
 
 
@@ -92,38 +92,41 @@ namespace CustomListTests
         //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
         [TestMethod]
         public void Constructor_InitalizeList_CapacityStartsAtFour()
-        {   
+        {
             //Arrange 
             CustomList<int> TestList = new CustomList<int>();
             int ExpectedCapcity = 4;
-            
+
             //Act
             int actual = TestList.Capacity;
-            
+
             //Assert
             Assert.AreEqual(actual, ExpectedCapcity);
         }
         //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
-        [TestMethod] 
+        [TestMethod]
         public void Add_AddFourItems_CapacityDoublesWhenReached()
         {
             //Arrange 
 
-            
             CustomList<int> TestList = new CustomList<int>();
             int ExpectedCapcity = 8;
             int actual;
-            
+
 
             //Act
 
-            TestList.Add(0);
-            TestList.Add(0);
-            TestList.Add(0);
-            TestList.Add(0);
+            TestList.Add(1);
+            TestList.Add(2);
+            TestList.Add(3);
+            TestList.Add(4);
+
+
+
             actual = TestList.Capacity;
 
             //Assert
+
             Assert.AreEqual(actual, ExpectedCapcity);
 
             //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
@@ -145,12 +148,12 @@ namespace CustomListTests
             TestList.Add(0);
             TestList.Add(0);
             TestList.Add(FifthItem);
-            actual = TestList[4]; 
+            actual = TestList[4];
 
             //Assert
             Assert.AreEqual(actual, FifthItem);
 
-
+            //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
         }
         [TestMethod]
         public void Add_AddFiveItems_ThirdItemAtSecondIndex()
@@ -174,6 +177,83 @@ namespace CustomListTests
             //Assert
             Assert.AreEqual(actual, thirdItem);
 
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+            //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
+        }
+        [TestMethod]
+        public void Remove_RemoveOneItemFromList_CountGoesDownByOne()
+        {
+            //Arrange 
+
+            CustomList<int> TestList = new CustomList<int>();
+            int expected = 3;
+            int actual;
+
+            //Act
+
+            TestList.Add(1);
+            TestList.Add(2);
+            TestList.Add(3);
+            TestList.Add(4);
+            expected = TestList.Count - 1;
+            TestList.Remove(3);
+            actual = TestList.Count;
+
+            //Assert
+
+            Assert.AreEqual(actual, expected);
+
+
+            //MethodBeingTested_WhatWeAreDoingInsideThatMethod_WhatWeExpectTheResultToBe
+        }
+        [TestMethod]
+        public void Remove_RemoveDuplicateValue_OnlyRemoveFirstValueOfDuplicate()
+        {
+            //Arrange 
+            CustomList<int> TestList = new CustomList<int>();
+            int duplicateItem1 = 3;
+            int expected = 3;
+            int actual;
+
+            //Act
+            TestList.Add(23);
+            TestList.Add(10);
+            TestList.Add(duplicateItem1);
+            TestList.Add(30);
+            TestList.Add(duplicateItem1);
+            TestList.Remove(duplicateItem1);
+            actual = TestList[3];
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+
+        }
+        [TestMethod]
+        public void Remove_RemoveFirstItem_HasItemTwoReplacedItemOne()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int expected = 0;
+            int actual;
+
+            //Act
+            customList.Add(1); //index 0
+            customList.Add(expected); //index 1
+            customList.Add(3); //index 2
+            customList.Remove(1);
+            customList.Add(expected);
+            actual = customList[0]; 
+
+
+            //Assert
+
+            Assert.AreEqual(actual, expected);
 
         }
 
--- CustomListUnitTestStarter/CustomList.cs
+++ CustomListUnitTestStarter/CustomList.cs
@@ -54,5 +54,20 @@ namespace CustomListUnitTestStarter
 
 
         }
+        public void Remove(T item)
+        {
+            // Check if we need to expand the array.
+            if (Count - 1 >= Capacity)
+            {
+                T[] tempArray = new T[Capacity * 2]; // Creating a new array, adding onto the capacity.
+                for (int i = 0; i < items.Length; i--) // Loop to copy items into the replacement array.
+                {
+                    tempArray[i] = items[i];
+                }
+                items = tempArray; // Replacing the old array with the new array.
+            }
+            items[Count] = item; // Set item into items array at the count index.
+            Count--;  // Remove one to the count.
+        }
     }
 }
